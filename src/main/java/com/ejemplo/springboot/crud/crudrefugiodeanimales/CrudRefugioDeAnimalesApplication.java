47bab783e3cf6289be2dd526f948dbb0ab233ab2
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2024-04-13 05:30:17 -0300

    Initial commit

---
+++ src/main/java/com/ejemplo/springboot/crud/crudrefugiodeanimales/CrudRefugioDeAnimalesApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.springboot.crud.crudrefugiodeanimales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRefugioDeAnimalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRefugioDeAnimalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ejemplo/springboot/crud/crudrefugiodeanimales/CrudRefugioDeAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.springboot.crud.crudrefugiodeanimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRefugioDeAnimalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
